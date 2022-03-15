82b5d9fea51145933433afc2cf3a830720f6422c
Adil Tuladhar <adil@truemark.com.np>
2022-03-15 04:03:48 +0000

    Validating user credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ require 'constants'
 
 class User < ApplicationRecord
   include Constants::AppEnums
+  PASSWORD_FORMAT = /\A
+  (?=.{8,})          # Must contain 8 or more characters
+  (?=.*\d)           # Must contain a digit
+  (?=.*[a-z])        # Must contain a lower case character
+  (?=.*[A-Z])        # Must contain an upper case character
+  (?=.*[[:^alnum:]]) # Must contain a symbol
+/x
 
   devise :registerable, :recoverable, :rememberable,
          :validatable, :confirmable,
@@ -11,4 +18,7 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   enum role: { admin: ADMIN, audience: AUDIENCE }
+
+  validates :first_name, :last_name, :role, presence: true
+  validates :password, format: { with: PASSWORD_FORMAT }
 end
